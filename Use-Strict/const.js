a38ad76e42d435419bd948a046cdd82999ae4cf0
Mark <mark_putyato@mail.ru>
2020-12-05 16:25:03 +0300

    Use strict, variables, consts. Initial Commit.

---
+++ Use-Strict/const.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const MY_BIRTHDAY = "26.08.2002";
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// low-case letter, it's being calculated during script execution: let COLOR is incorrect
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
---
+++ Use-Strict/strict.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let message;
+
+message = "Hello, JS!";
+alert(message);
+
+let message1 = "Hello, Mark!";
+alert(message1);
+
+let user = "m0rk4", age = 18, message3 = "Hello, m0rk4!";
+
+var x = 1; // old declaration
+
+// copying
+
+message1 = message;
+alert(message1);
+
+// regexp for names [^0-9][0-9A-Za-z$_]*
+
+let _var, $exp, _, $, m1;
+// 1var - incorrect
